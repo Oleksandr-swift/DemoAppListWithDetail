50bb0324055bd2fdf2d243d55aa2cbe1140eb707
Aleksandr R <roditelev2020@icloud.com>
2022-09-30 13:12:31 +0300

    add new screen

---
+++ TableView/ViewControllers/DetailScreenViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailScreenViewController.swift
+//  TableView
+//
+//  Created by Admin on 30.09.2022.
+//
+
+import UIKit
+
+class DetailScreenViewController: UIViewController {
+
+    @IBOutlet weak var detailImageView: UIImageView!
+    @IBOutlet weak var fullDescriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
