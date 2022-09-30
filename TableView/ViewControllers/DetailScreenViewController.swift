17b2ce8c580342e8f4d77abc6618a836c0216ece
Aleksandr R <roditelev2020@icloud.com>
2022-10-01 00:17:20 +0300

    refactoring

--- TableView/ViewControllers/DetailScreenViewController.swift
+++ TableView/ViewControllers/DetailScreenViewController.swift
@@ -11,11 +11,14 @@ class DetailScreenViewController: UIViewController {
     
     @IBOutlet weak var detailImageView: UIImageView!
     @IBOutlet weak var fullDescriptionLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
+   
     var sunset: Sunset?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupVC()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,7 +27,15 @@ class DetailScreenViewController: UIViewController {
             setupData(sunset: sunset)
         }
     }
-   
+    
+    func setupVC() {
+        titleLabel.text = "Sunset"
+        titleLabel.font = UIFont(name: "System", size: 20)
+        titleLabel.textColor = .black
+        detailImageView.layer.cornerRadius = 15
+        detailImageView.contentMode = .scaleToFill
+    }
+    
     @IBAction func backButton(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
