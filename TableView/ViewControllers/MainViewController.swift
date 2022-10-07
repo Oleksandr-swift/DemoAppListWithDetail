9712bee60a7a8754917f5e4c772a785647499bb8
Aleksandr R <roditelev2020@icloud.com>
2022-10-08 00:07:06 +0300

    add segment control

--- TableView/ViewControllers/MainViewController.swift
+++ TableView/ViewControllers/MainViewController.swift
@@ -12,6 +12,7 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     @IBOutlet weak var informationTableView: UITableView!
     @IBOutlet weak var navigationBarView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var controlSegment: UISegmentedControl!
     
     let data: [Sunset] = [
         Sunset(title: "Morning Run", imageName: "sunset1", country: "Italy"),
@@ -19,6 +20,14 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         Sunset(title: "Vacation Photos", imageName: "sunset3", country: "Poland"),
         Sunset(title: "Visiting Frieends", imageName: "sunset4", country: "Czech Republic"),
         Sunset(title: "Some Other Title", imageName: "sunset5", country: "Сroatia"),
+    ]
+    
+    let newData: [Sunset] = [
+        Sunset(title: "Sunrise in the field", imageName: "sunrise1", country: "Norway"),
+        Sunset(title: "Beautiful sunrise in the mountains", imageName: "sunrise2", country: "Finland"),
+        Sunset(title: "Winter sunrise", imageName: "sunrise3", country: "Romania"),
+        Sunset(title: "Sunny dawn in a beautiful field", imageName: "sunrise4", country: "Sweden"),
+        Sunset(title: "Sunrise on the seaside", imageName: "sunrise5", country: "Austria"),
     ]
     
     override func viewDidLoad() {
@@ -31,8 +40,10 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         informationTableView.delegate = self
         titleLabel.text = "Sunset list"
         navigationBarView.backgroundColor = UIColor.lightGray
+        controlSegment.setTitle("Sunset", forSegmentAt: 0)
+        controlSegment.setTitle("Sunrise", forSegmentAt: 1)
     }
-   
+    
     private func navigateToDetailScreen(sunset: Sunset) {
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         let vc = storyboard.instantiateViewController(identifier: "Detail") as DetailScreenViewController
@@ -42,23 +53,26 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return controlSegment.selectedSegmentIndex == 0 ? data.count : newData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let sunset = data[indexPath.row]
+        let sunset = controlSegment.selectedSegmentIndex == 0 ? data[indexPath.row] : newData[indexPath.row]
         let cell = informationTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
         cell.setupUI(data: sunset)
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let sunset = self.data[indexPath.row]
+        let sunset = controlSegment.selectedSegmentIndex == 0 ? data[indexPath.row] : newData[indexPath.row]
         self.navigateToDetailScreen(sunset: sunset)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140
     }
+    @IBAction func didChangeSegment(_ sender: UISegmentedControl) {
+        informationTableView.reloadData()
+    }
 }
-    
+
